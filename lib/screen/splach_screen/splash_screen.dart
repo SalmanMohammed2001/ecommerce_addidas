0026c1e86e99a884f4c3bcc730f1afe2ad019f35
salman <salmannisthar123@gmail.com>
2024-08-07 10:41:19 +0530

    Initial commit

---
+++ lib/screen/splach_screen/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/adidas-logo.png"),
+            const CupertinoActivityIndicator(
+              color: Colors.black,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
