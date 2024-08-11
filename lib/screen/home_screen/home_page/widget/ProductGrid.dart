fbbae7916bcb1b30a5edcd9859aab898463775dd
salman <salmannisthar123@gmail.com>
2024-08-11 13:49:39 +0530

    add shimmer package

--- lib/screen/home_screen/home_page/widget/ProductGrid.dart
+++ lib/screen/home_screen/home_page/widget/ProductGrid.dart
@@ -1,84 +1,92 @@
-
-
 import 'package:ecommerce_addidas/controller/product_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../model/SneakerModel.dart';
 
 class ProductGrid extends StatelessWidget {
   const ProductGrid({
     super.key,
-
   });
-
-
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: ProductController().fetchProduct(),
-      builder: (context,snapshot) {
-        if(snapshot.hasError){
-          return const Text("Has Error");
-        }
+        future: ProductController().fetchProduct(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Has Error");
+          }
 
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return const CircularProgressIndicator();
-        }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return GridView.builder(
+              itemCount: 6,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(crossAxisSpacing: 10, mainAxisSpacing: 10, crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return Shimmer.fromColors(highlightColor: Colors.grey.shade500,
 
-        List<SneakerModel> sneaker=snapshot.data!;
-
-
-
-        return GridView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: sneaker.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return Container(
-              height: 100,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(sneaker[index].image),
-                    fit: BoxFit.cover),
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Stack(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Chip(
-                          label: Text("LKR ${sneaker[index].price}0"),
-                        ),
-                        const Icon(
-                            color: Colors.grey,
-                            Icons.favorite_outline_rounded)
-                      ],
-                    ),
-                    Positioned(
-                        bottom: 5,
-                        child: Text(
-                          sneaker[index].title,
-                          style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500),
-                          overflow: TextOverflow.ellipsis,
-                        ))
-                  ],
-                ),
-              ),
+                    baseColor: Colors.grey.shade400,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                    ));
+              },
             );
-          },
-        );
-      }
-    );
+          }
+
+          List<SneakerModel> sneaker = snapshot.data!;
+
+          return GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: sneaker.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 10, mainAxisSpacing: 10, crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(sneaker[index].image),
+                      fit: BoxFit.cover),
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Chip(
+                            label: Text("LKR ${sneaker[index].price}0"),
+                          ),
+                          const Icon(
+                              color: Colors.grey,
+                              Icons.favorite_outline_rounded)
+                        ],
+                      ),
+                      Positioned(
+                          bottom: 5,
+                          child: Text(
+                            sneaker[index].title,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w500),
+                            overflow: TextOverflow.ellipsis,
+                          ))
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        });
   }
 }
